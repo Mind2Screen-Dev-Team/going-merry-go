da507df74e9db34482b50a3ce3c4be80c989f804
Muhammmad Ali Zufan <muhammadalizufan@gmail.com>
2024-10-20 21:22:26 +0700

    upgrade: pkg depedency

--- app/app_startup.go
+++ app/app_startup.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"fmt"
 	"slices"
 
 	"github.com/Mind2Screen-Dev-Team/go-skeleton/app/registry"
@@ -14,10 +13,10 @@ func Startup(service string, reg *registry.AppRegistry, fns ...func()) error {
 
 	if slices.Contains([]string{"rest-api", "grpc-api"}, service) {
 		// # Load MySQL First
-		reg.Dependency.MySqlDB.Value()
-		if err := reg.Dependency.MySqlDB.Error(); err != nil {
-			return fmt.Errorf("failed to load depedency mysql db: %w", err)
-		}
+		// reg.Dependency.MySqlDB.Value()
+		// if err := reg.Dependency.MySqlDB.Error(); err != nil {
+		// 	return fmt.Errorf("failed to load depedency mysql db: %w", err)
+		// }
 	}
 
 	return nil
